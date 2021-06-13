c600da50c9fbcf50e22953942e9b69fa939e0692
Koki <tanakamaruko1121n@gmail.com>
2021-06-14 08:30:31 +0900

    Wrong beginner 47 B

---
+++ beginner_47_B.py
@@ -0,0 +1,19 @@
+W, H, N = map(int, input().split())
+
+xyas = []
+for i in range(N):
+    xyas.append(list(map(int, input().split())))
+
+x_min, x_max, y_min, y_max = (0, W, 0, H)
+
+for xya in xyas:
+    if xya[2] == 1:
+        x_min = xya[0]
+    elif xya[2] == 2:
+        x_max = xya[0]
+    elif xya[2] == 3:
+        y_min = xya[1]
+    else:
+        y_max = xya[1]
+
+print(max(0, max(0, (x_max - x_min)) * max(0, (y_max - y_min))))
