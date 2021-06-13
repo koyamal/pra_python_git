b6b25ecdf2641cbea401285b5989a3e5e45cc862
Koki <tanakamaruko1121n@gmail.com>
2021-06-14 08:45:10 +0900

    Solve beginner 47 B

--- beginner_47_B.py
+++ beginner_47_B.py
@@ -8,12 +8,17 @@ x_min, x_max, y_min, y_max = (0, W, 0, H)
 
 for xya in xyas:
     if xya[2] == 1:
-        x_min = xya[0]
+        if x_min < xya[0]:
+            x_min = xya[0]
     elif xya[2] == 2:
-        x_max = xya[0]
+        if x_max > xya[0]:
+            x_max = xya[0]
     elif xya[2] == 3:
-        y_min = xya[1]
+        if y_min < xya[1]:
+            y_min = xya[1]
     else:
-        y_max = xya[1]
+        if y_max > xya[1]:
+            y_max = xya[1]
 
-print(max(0, max(0, (x_max - x_min)) * max(0, (y_max - y_min))))
+
+print(max(0, (x_max - x_min)) * max(0, (y_max - y_min)))
