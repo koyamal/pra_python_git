6dbe4fcc051fff3b7fd1255ff94f926ad720704f
Koki <tanakamaruko1121n@gmail.com>
2021-06-19 18:19:50 +0900

    Solve beginner 51 B

--- beginner_51_B.py
+++ beginner_51_B.py
@@ -3,10 +3,8 @@ K, S = map(int, input().split())
 count = 0
 for x in range(K + 1):
     for y in range(K + 1):
-        for z in range(K + 1):
-            if x + y + z == S:
-                count = count + 1
-
+        if K >= S - x - y >= 0:
+            count = count + 1
 
 
 print(count)
