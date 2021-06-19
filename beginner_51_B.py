18ecc006aa4bdf22d1d03c7b7a70c41b7f511fc0
Koki <tanakamaruko1121n@gmail.com>
2021-06-19 18:15:37 +0900

    TLE beginner 51 B

--- beginner_51_B.py
+++ beginner_51_B.py
@@ -1,2 +1,12 @@
 K, S = map(int, input().split())
 
+count = 0
+for x in range(K + 1):
+    for y in range(K + 1):
+        for z in range(K + 1):
+            if x + y + z == S:
+                count = count + 1
+
+
+
+print(count)
