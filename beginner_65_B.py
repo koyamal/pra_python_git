b7bce44cb46345fcc4a5b4998c20595130ecbbc5
Koki <tanakamaruko1121n@gmail.com>
2021-07-02 08:52:15 +0900

    TLE beginner 65 B

---
+++ beginner_65_B.py
@@ -0,0 +1,23 @@
+N = int(input())
+listA = []
+indexShinedLight = [1]
+for i in range(N):
+    listA.append(int(input()))
+
+shineSecond = True
+
+tempIndex = 1
+while shineSecond:
+    tempIndex = listA[tempIndex - 1]
+    if tempIndex in indexShinedLight:
+        shineSecond = False
+        print(-1)
+        exit()
+
+    if tempIndex == 2:
+        shineSecond = False
+        print(len(indexShinedLight))
+        exit()
+
+    indexShinedLight.append(tempIndex)
+
