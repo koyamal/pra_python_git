5ab367336ea916ad09020227b5b82d692548e729
Koki <tanakamaruko1121n@gmail.com>
2021-07-07 08:40:43 +0900

    Solve beginner 70 B

--- beginner_70_B.py
+++ beginner_70_B.py
@@ -1,8 +1,8 @@
 A, B, C, D = map(int, input().split())
 
 output = 0
-for i in range(C, D):
+for i in range(C, D + 1):
     if A <= i <= B:
         output += 1
 
-print(output)
+print(max(output - 1, 0))
