f7ec6b05a689bb88ed5f8d2345a12d3926499cd5
Koki <tanakamaruko1121n@gmail.com>
2021-07-15 22:32:27 +0900

    Solve beginner 79 B

--- beginner_79_B.py
+++ beginner_79_B.py
@@ -1,14 +1,24 @@
 N = int(input())
 
-def cal_L(target):
+
+def cal_L(target, result):
     if target == 0:
         return 2
     if target == 1:
         return 1
 
-    return cal_L(target - 1) + cal_L(target - 2)
+    if target - 1 not in result:
+        result[target - 1] = cal_L(target - 1, result)
+
+    if target - 2 not in result:
+        result[target - 2] = cal_L(target - 2, result)
+
+    return result[target - 1] + result[target - 2]
+
+
+result_L = {0: 2, 1: 1}
 
 if N == 1:
     print('1')
 else:
-    print(cal_L(N))
+    print(cal_L(N, result_L))
