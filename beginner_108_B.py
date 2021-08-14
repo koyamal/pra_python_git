4d37c2fb0333f8496a88d9a4bfed3574465f2f7f
koyamal <tanakamaruko1121n@gmail.com>
2021-08-14 12:13:25 +0900

    Solve beginner 108 B

--- beginner_108_B.py
+++ beginner_108_B.py
@@ -1,27 +1,11 @@
 x1, y1, x2, y2 = map(int, input().split())
 
-x3, y3, x4, y4 = 0, 0, 0, 0
-if x1 == x2:
-    if y1 > y2:
-        x3 = x2 + (y1 - y2)
-        y3 = y2
-        x4 = x3
-        y4 = x1
-    else:
-        x3 = x2 - (y2 - y1)
-        y3 = y2
-        x4 = x3
-        y4 = y1
-else:
-    if x1 > x2:
-        x3 = x2
-        y3 = y2 - (x1 - x2)
-        x4 = x1
-        y4 = y3
-    else:
-        x3 = x2
-        y3 = y2 + (x2 - x1)
-        x4 = x1
-        y4 = y3
+A = x2 - x1
+B = y2 - y1
+
+x3 = x2 - B
+y3 = y2 + A
+x4 = x1 - B
+y4 = y1 + A
 
 print(x3, y3, x4, y4)
