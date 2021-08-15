0e4e3dabda029dbfce089ab6325c3c6eef63b454
koyamal <tanakamaruko1121n@gmail.com>
2021-08-15 16:04:50 +0900

    Study python 20210815

---
+++ study_python_20210815.py
@@ -0,0 +1,35 @@
+# squares = [1, 4, 9, 16, 25]
+# copy_squares = squares[:]
+# print("squares", squares)
+# print("copy", copy_squares)
+#
+# copy_squares[0] = 0
+# print("squares", squares)
+# print("copy", copy_squares)
+
+# words = ["cat", "window", "defenestrate"]
+# for w in words[:]:
+#     print(w)
+#     if len(w) > 6:
+#         words.insert(0, w)
+# print(words)
+
+# print(range(5))
+# print(list(range(5)))
+
+# def functions(word="Hello", *args, **kwargs):
+#     print(word)
+#     for arg in args:
+#         print("arg :", arg)
+#     for key in kwargs.keys():
+#         print(key, ":", kwargs[key])
+#
+# functions("GM!", "Tom", "Jhon", dad="DADY", mom="MOMY")
+
+# l = ["July", "Maru", "Koki", "Jack"]
+# l_sorted_second = sorted(l, key=lambda x: x[1])
+# print(l_sorted_second)
+
+# x, y, z = "tes"
+# x, y, z = ["a", "b", "c"]
+# print("x:", x, "y:", y, "z:", z)
