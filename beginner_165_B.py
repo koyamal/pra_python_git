c7481b6b0d1838acddb218b04872ffa56efa640b
koyamal <tanakamaruko1121n@gmail.com>
2021-09-03 08:56:03 +0900

    Wrong beginner 165 B

--- beginner_165_B.py
+++ beginner_165_B.py
@@ -4,7 +4,8 @@ output = 1
 nowMoney = 100
 
 while nowMoney < X:
-    nowMoney = int(nowMoney * 1.01)
+    #nowMoney = int(nowMoney * 1.01)
+    nowMoney += int(nowMoney * 0.01)
     output += 1
 
 print(output - 1)
