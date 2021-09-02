a656d4fa3c19d426d58846ead823280b0ae47214
koyamal <tanakamaruko1121n@gmail.com>
2021-09-03 08:57:52 +0900

    Solve beginner 165 B

--- beginner_165_B.py
+++ beginner_165_B.py
@@ -5,7 +5,8 @@ nowMoney = 100
 
 while nowMoney < X:
     #nowMoney = int(nowMoney * 1.01)
-    nowMoney += int(nowMoney * 0.01)
+    #nowMoney += int(nowMoney * 0.01)
+    nowMoney += nowMoney // 100
     output += 1
 
 print(output - 1)
