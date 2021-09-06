917f6829e830e0b2254768f8be3b9488a7006129
koyamal <tanakamaruko1121n@gmail.com>
2021-09-07 08:49:59 +0900

    Solve beginner 175 A

--- beginner_175_A.py
+++ beginner_175_A.py
@@ -1,18 +1,17 @@
 S = input()
 
-pS = S[0]
+pS = None
 count = 0
 output = 0
-for i in range(1, len(S)):
+for i in range(len(S)):
     if pS == S[i] == "R":
         count += 1
+    elif (pS is None and S[i] == "R") or (pS == "S" and S[i] == "R"):
+        count = 1
     else:
         count = 0
     if count > output:
         output = count
     pS = S[i]
 
-if output == 0:
-    print(output)
-else:
-    print(output + 1)
+print(output)
