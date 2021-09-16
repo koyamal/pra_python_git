411ba9cbf26db3a7f14b9bdf1161102ea684fad6
koyamal <tanakamaruko1121n@gmail.com>
2021-09-17 00:02:17 +0900

    Solve beginner 187 B

--- beginner_187_B.py
+++ beginner_187_B.py
@@ -2,12 +2,12 @@ N = int(input())
 
 xy = []
 for _ in range(N):
-    x, y = map(int, input())
+    x, y = map(int, input().split())
     xy.append([x, y])
 
 output = 0
 for i in range(N - 1):
-    for j in range(N):
+    for j in range(i + 1, N):
         if -1 <= (xy[i][1] - xy[j][1]) / (xy[i][0] - xy[j][0]) <= 1:
             output += 1
 
