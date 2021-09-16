c77b2d98ab1794d8b5f25e21cd030f120a65ebef
koyamal <tanakamaruko1121n@gmail.com>
2021-09-16 23:59:44 +0900

    RE beginner 187 B

---
+++ beginner_187_B.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+xy = []
+for _ in range(N):
+    x, y = map(int, input())
+    xy.append([x, y])
+
+output = 0
+for i in range(N - 1):
+    for j in range(N):
+        if -1 <= (xy[i][1] - xy[j][1]) / (xy[i][0] - xy[j][0]) <= 1:
+            output += 1
+
+print(output)
