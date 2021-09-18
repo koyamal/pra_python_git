fc2c70561185fc39f3ed7788ba6467e0927949b9
koyamal <tanakamaruko1121n@gmail.com>
2021-09-18 22:22:42 +0900

    Wrong beginner 189 B

--- beginner_189_B.py
+++ beginner_189_B.py
@@ -3,7 +3,7 @@ N, X = map(int, input().split())
 sumA = 0
 for i in range(N):
     V, P = map(int, input().split())
-    sumA += int(V * P / 100)
+    sumA += V * P * 0.01
     if sumA > X:
         print(i + 1)
         exit()
