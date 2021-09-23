7994f15223e51e1f0fadd70338b7610d6d699ab8
koyamal <tanakamaruko1121n@gmail.com>
2021-09-23 09:50:38 +0900

    Solve beginner 194 B

--- beginner_194_B.py
+++ beginner_194_B.py
@@ -11,7 +11,7 @@ Bs.sort()
 
 if As[0][1] == Bs[0][1]:
     if As[0][0] + Bs[0][0] >= min(As[1][0], Bs[1][0]):
-        print(min(As[1][0], Bs[1][0]))
+        print(max(max(As[0][0], Bs[0][0]), min(As[1][0], Bs[1][0])))
     else:
         print(As[0][0] + Bs[0][0])
 else:
