ea33686ae8820c4bc14a674360ee0926b50b0402
koyamal <tanakamaruko1121n@gmail.com>
2021-09-23 09:43:56 +0900

    Wrong beginner 194 B

---
+++ beginner_194_B.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+As = []
+Bs = []
+for i in range(N):
+    A, B = map(int, input().split())
+    As.append([A, i])
+    Bs.append([B, i])
+As.sort()
+Bs.sort()
+
+if As[0][1] == Bs[0][1]:
+    if As[0][0] + Bs[0][0] >= min(As[1][0], Bs[1][0]):
+        print(min(As[1][0], Bs[1][0]))
+    else:
+        print(As[0][0] + Bs[0][0])
+else:
+    print(max(As[0][0], Bs[0][0]))
