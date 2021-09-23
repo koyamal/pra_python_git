0bf716b4386ecf0312ecf7a204f917f363f9708d
koyamal <tanakamaruko1121n@gmail.com>
2021-09-23 09:22:46 +0900

    Fix beginner 193 B

--- beginner_193_B.py
+++ beginner_193_B.py
@@ -1,10 +1,10 @@
 N = int(input())
 
-minP = 10 ** 9
+minP = 10 ** 9 + 1
 canBuy = False
 for _ in range(N):
     A, P, X = map(int, input().split())
-    if X - A > 0 and minP >= P:
+    if X - A > 0 and minP > P:
         minP = P
         canBuy = True
 
