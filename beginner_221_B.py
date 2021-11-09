0b8b3182b77728d212d100ad471cac2b1669c08e
koyamal <tanakamaruko1121n@gmail.com>
2021-11-09 22:09:58 +0900

    Solve beginner 221 B

--- beginner_221_B.py
+++ beginner_221_B.py
@@ -1,17 +1,16 @@
 S = input()
 T = input()
 
-hp = 1
+hp = 0
+count = 0
 for i in range(len(S) - 1):
     if S[i] != T[i]:
-        if S[i] == T[i + 1] and S[i + 1] == T[i]:
-            hp -= 1
+        if S[i] == T[i + 1] and S[i + 1] == T[i] and hp == 0:
+            hp += 1
         else:
-            print("No")
-            exit()
+            count += 1
 
-if S == T or hp == 0:
+if S == T or (hp == 1 and count <= 1):
     print("Yes")
 else:
     print("No")
-    
