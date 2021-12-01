c2269fd85fd0a3a141bf996feb6ce0e9c5c9d786
koyamal <tanakamaruko1121n@gmail.com>
2021-12-02 08:42:44 +0900

    Create pra_datetime

--- pra_datetime.py
+++ pra_datetime.py
@@ -1,5 +1,23 @@
 import datetime
 
-dt_now = datetime.datetime.now()
 
-print(type(dt_now))
+def pra_datetime():
+    # 現在時刻
+    dt_now = datetime.datetime.now()
+    print("======= datetime(dt_now) =======")
+    print(type(dt_now))
+    print("dt_now: ", dt_now)
+    print("year: ", dt_now.year)
+    print("hour: ", dt_now.hour)
+
+    # datetimeのコンストラクタ(year,month,dayは必須、hour以下は省略可)
+    dt = datetime.datetime(1993, 11, 21)
+    print("======= datetime(dt) =======")
+    print(type(dt))
+    print("dt_now: ", dt)
+    print("year: ", dt.year)
+    print("hour: ", dt.hour)
+
+
+if __name__ == "__main__":
+    pra_datetime()
