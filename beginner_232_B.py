a032b7657dc5780276cf2266c3a53675860eb024
koyamal <tanakamaruko1121n@gmail.com>
2022-01-13 23:49:13 +0900

    Wrong beginner 232 B

--- beginner_232_B.py
+++ beginner_232_B.py
@@ -1,10 +1,10 @@
 S = input()
 T = input()
 
-diff = ord(S[0]) - ord(T[0])
+diff = ord(T[0]) - ord(S[0])
 
 for i in range(len(S)):
-    if str(ord(S[i]) - diff) != T[0]:
+    if str(ord(S[i]) + diff) != T[0]:
         print("No")
         exit()
 
