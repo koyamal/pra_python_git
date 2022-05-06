018be17e3df202d81f89b8aff1dd2df682be3447
koyamal <tanakamaruko1121n@gmail.com>
2022-05-06 20:45:38 +0900

    Solve beginner 249 A

--- beginner_249_A.py
+++ beginner_249_A.py
@@ -3,7 +3,7 @@ A, B, C, D, E, F, X = map(int, input().split())
 numT = X //(A + C)
 remT = X % (A + C)
 numA = X // (D + F)
-remA = X % (A + C)
+remA = X % (D + F)
 
 disT = numT * B * A + B * min(remT, A)
 disA = numA * E * D + E * min(remA, D)
