472b89d8998af213df5dcf6b2c4073249ac1ca0f
koyamal <tanakamaruko1121n@gmail.com>
2022-06-18 21:44:38 +0900

    Solve beginner 255 B

--- beginner_255_B.py
+++ beginner_255_B.py
@@ -6,13 +6,12 @@ for _ in range(N):
     XYs.append([X, Y])
 
 max = 0
-for i in range(K):
-    min = 10 ** 10
-    for j in range(N):
-        if(As[i] - 1 != j):
-            if (XYs[As[i] - 1][0] - XYs[j][0]) ** 2 + (XYs[As[i] - 1][1] - XYs[j][1]) ** 2 < min:
-                min = (XYs[As[i] - 1][0] - XYs[j][0]) ** 2 + (XYs[As[i] - 1][1] - XYs[j][1]) ** 2
+for i in range(N):
+    min = 10 ** 10 ** 2
+    for j in range(K):
+            if (XYs[As[j] - 1][0] - XYs[i][0]) ** 2 + (XYs[As[j] - 1][1] - XYs[i][1]) ** 2 < min:
+                min = (XYs[As[j] - 1][0] - XYs[i][0]) ** 2 + (XYs[As[j] - 1][1] - XYs[i][1]) ** 2
     if min > max:
         max = min
 
-print(min ** 0.5)
+print(max ** 0.5)
