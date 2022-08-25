edd62a6f55c7f844dc714a42b5de7e125991b342
koyamal <tanakamaruko1121n@gmail.com>
2022-08-25 22:26:54 +0900

    Refactor code

--- can_ana.py
+++ can_ana.py
@@ -2,8 +2,7 @@ with open('/Users/koki/Downloads/dump.log') as f:
     for line in f:
         line = line.split(' ')
         id = line[2].split('#')[0]
-        # print(id)
-        if(id == "244"):
+        if id == "244":
             print(id)
             data = line[2].split('#')[1]
             f = open('/Users/koki/Downloads/dump.out.log3', 'a')
