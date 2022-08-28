aa6e9ebc420f40d0ba624c379ee721c379af7efc
koyamal <tanakamaruko1121n@gmail.com>
2022-08-28 22:54:28 +0900

    Rename val

--- can_ana.py
+++ can_ana.py
@@ -1,8 +1,8 @@
 with open('/Users/koki/Downloads/dump.log') as f:
     for line in f:
         line = line.split(' ')
-        id = line[2].split('#')[0]
-        if id == "244":
+        nid = line[2].split('#')[0]
+        if nid == "244":
             data = line[2].split('#')[1]
             f = open('/Users/koki/Downloads/dump.out.log3', 'a')
             f.write(data)
