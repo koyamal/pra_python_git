86741fc49f8e2db7ebce247f33b58be10ec1a28d
koyamal <tanakamaruko1121n@gmail.com>
2022-09-01 22:38:39 +0900

    Refactor code

--- can_ana02.py
+++ can_ana02.py
@@ -1,5 +1,5 @@
 import codecs
-output = ""
+
 with open('/Users/koki/Downloads/dump.out.log3') as f:
     for line in f:
         for i in range(int(len(line) / 2)):
@@ -7,9 +7,5 @@ with open('/Users/koki/Downloads/dump.out.log3') as f:
                 if str(line[2 * i]) + str(line[2 * i + 1]) != "00":
                     data = str(codecs.decode(str(line[2 * i]) + str(line[2 * i + 1]), "hex"), 'utf-8')
                     print(data)
-                    if output[len(output) - 1:] != data:
-                        output = output + data
             except ValueError as e:
                 pass
-
-print(output)
