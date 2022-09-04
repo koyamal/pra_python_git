38c503d745c2297225b9f8af6fd8f47d6c6d7bcd
koyamal <tanakamaruko1121n@gmail.com>
2022-09-04 12:15:03 +0900

    Refactor code

--- can_ana.py
+++ can_ana.py
@@ -3,7 +3,7 @@ with open(path + 'dump.log') as f:
     for line in f:
         line = line.split(' ')
         nid = line[2].split('#')
-        if nid[0] == "244":
+        if nid[0] == '244':
             data = nid[1]
             fw = open(path + 'dump.out.log3', 'a')
             fw.write(data)
