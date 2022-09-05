51bb7880b30301939cc7efa6279bbd5c326e7cb7
koyamal <tanakamaruko1121n@gmail.com>
2022-09-05 23:12:33 +0900

    Refactor code

--- can_ana.py
+++ can_ana.py
@@ -1,8 +1,8 @@
 path = '/Users/koki/Downloads/'
 with open(path + 'dump.log') as f:
     for line in f:
-        line = line.split(' ')
-        nid = line[2].split('#')
+        lData = line.split(' ')
+        nid = lData[2].split('#')
         if nid[0] == '244':
             data = nid[1]
             fw = open(path + 'dump.out.log3', 'a')
