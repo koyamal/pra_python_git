0e510170427cda46993c08d83f1655ff031750e5
koyamal <tanakamaruko1121n@gmail.com>
2022-09-09 23:43:17 +0900

    TLE beginner 262 B again

--- beginner_262_B.py
+++ beginner_262_B.py
@@ -7,9 +7,11 @@ for _ in range(M):
 output = 0
 for i in range(M - 2):
     for j in range(i + 1, M - 1):
-        for k in range(j + 1, M):
-            s = set(UV[i] + UV[j] + UV[k])
-            if len(s) == 3:
-                output += 1
+        s = set(UV[i] + UV[j])
+        if len(s) != 4:
+            for k in range(j + 1, M):
+                ss = set(UV[i] + UV[j] + UV[k])
+                if len(ss) == 3:
+                    output += 1
 
 print(output)
