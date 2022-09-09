ccc5e3b62b951dbe806e26422c27fdf98393a6f0
koyamal <tanakamaruko1121n@gmail.com>
2022-09-10 00:20:15 +0900

    Wrong beginner 262 B

--- beginner_262_B.py
+++ beginner_262_B.py
@@ -7,12 +7,21 @@ for _ in range(M):
 output = 0
 for i in range(M - 2):
     for j in range(i + 1, M - 1):
-        s = set(UV[i] + UV[j])
-        if len(s) != 4:
-            for k in range(j + 1, M):
-                ss = set(UV[i] + UV[j] + UV[k])
-                if len(ss) == 3:
-                    output += 1
-                    break
+        if UV[i][0] == UV[j][0]:
+            if ([UV[i][1], UV[j][1]] in UV) or ([UV[j][1], UV[i][1]] in UV):
+                output += 1
+                break
+        elif UV[i][0] == UV[j][1]:
+            if ([UV[i][1], UV[j][0]] in UV) or ([UV[j][0], UV[i][1]] in UV):
+                output += 1
+                break
+        elif UV[i][1] == UV[j][0]:
+            if ([UV[i][0], UV[j][1]] in UV) or ([UV[j][1], UV[i][0]] in UV):
+                output += 1
+                break
+        elif UV[i][1] == UV[j][1]:
+            if ([UV[i][0], UV[j][0]] in UV) or ([UV[j][0], UV[i][0]] in UV):
+                output += 1
+                break
 
 print(output)
