cba9e858cce0321cef3bde7c24c5931113e49bd2
koyamal <tanakamaruko1121n@gmail.com>
2022-09-27 21:21:51 +0900

    Edit split part

--- base64ToJpeg.py
+++ base64ToJpeg.py
@@ -3,7 +3,8 @@ import base64
 s = input()
 title = input()
 
-sb = s[23:].encode()
+#sb = s[23:].encode()
+sb = s.split(',')[1]
 
 img = base64.b64decode(sb)
 with open("./ebi/" + title + ".jpg", 'wb') as f4:
