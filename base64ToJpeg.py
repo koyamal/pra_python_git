232785f60e6990db00547a2453261ecc99ee4a3c
koyamal <tanakamaruko1121n@gmail.com>
2022-09-30 17:56:53 +0900

    Edit code

--- base64ToJpeg.py
+++ base64ToJpeg.py
@@ -4,8 +4,8 @@ s = input()
 title = input()
 
 #sb = s[23:].encode()
-sb = s.split(',')[1]
+#sb = s.split(',')[1]
 
-img = base64.b64decode(sb)
+img = base64.b64decode(s.split(',')[1])
 with open("./ebi/" + title + ".jpg", 'wb') as f4:
     f4.write(img)
