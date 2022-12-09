3c07822b6a6321e1ac5cdb19e9328950caa16ef0
koyamal <tanakamaruko1121n@gmail.com>
2022-12-09 21:41:06 +0900

    Edit code

--- baseTest.py
+++ baseTest.py
@@ -2,7 +2,7 @@ import base64
 print("Start!")
 with open("test.jpg", "rb") as f:
     img = f.read()
-
+print("Second point")
 img_base64 = base64.b64encode(img)
 
 with open("test_new.jpg", "wb") as f:
