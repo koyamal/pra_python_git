443345742f550f509eb3cf02ad5546044f5810f8
koyamal <tanakamaruko1121n@gmail.com>
2022-12-10 16:59:11 +0900

    Add third point

--- baseTest.py
+++ baseTest.py
@@ -4,7 +4,7 @@ with open("test.jpg", "rb") as f:
     img = f.read()
 print("Second point")
 img_base64 = base64.b64encode(img)
-
+print('Third point')
 with open("test_new.jpg", "wb") as f:
     f.write(base64.b64decode(img_base64))
 
