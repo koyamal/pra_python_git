0252a168b68e6affbf7067482761c587f86cce26
koyamal <tanakamaruko1121n@gmail.com>
2022-12-13 20:30:55 +0900

    Add for roop

--- baseTest.py
+++ baseTest.py
@@ -8,6 +8,11 @@ print('Third point')
 with open("test_new.jpg", "wb") as f:
     f.write(base64.b64decode(img_base64))
 
+data = [1, 3, 4, 5, 6]
+
+for d in data:
+    print(d)
+    
 print("4th point")
 
 print("Done")
