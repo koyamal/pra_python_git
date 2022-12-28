4a06707789b121820de62cd5a40603eb68576afd
koyamal <tanakamaruko1121n@gmail.com>
2022-12-28 16:21:22 +0900

    Add try point

--- can_ana02.py
+++ can_ana02.py
@@ -6,6 +6,7 @@ with open('/Users/koki/Downloads/dump.out.log3') as f:
     for line in f:
         for i in range(int(len(line) / 2)):
             try:
+                print("In try process")
                 strData = str(line[2 * i]) + str(line[2 * i + 1])
                 if strData != '00':
                     data = str(codecs.decode(strData, 'hex'), 'utf-8')
