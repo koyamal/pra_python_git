794d599c3c7a5eeef90a2cf80bdcf57743207c12
koyamal <tanakamaruko1121n@gmail.com>
2023-01-05 23:47:13 +0900

    Print test code

--- can_ana02.py
+++ can_ana02.py
@@ -16,6 +16,7 @@ with open('/Users/koki/Downloads/dump.out.log3') as f:
                     t2 = [5, 6]
                     a = [7, 8]
                     a2 = [7, 8]
+                    print("test code")
                     data = str(codecs.decode(strData, 'hex'), 'utf-8')
                     print(data)
             except ValueError as e:
