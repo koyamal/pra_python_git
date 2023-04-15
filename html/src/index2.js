8a25df72b01e440a36384fd751faf75ad1fff63c
koyamal <tanakamaruko1121n@gmail.com>
2023-04-15 17:36:24 +0900

    Edit color swich algo

--- html/src/index2.js
+++ html/src/index2.js
@@ -2,26 +2,34 @@ const memoListItems = document.querySelectorAll('.memo-list-item');
 
 console.log(memoListItems);
 
-let colorSwitch = false;
+// let colorSwitch = false;
+let colorSwitch = 0;
 
 const onClickColor = () => {
-    if(!colorSwitch){
-        colorSwitch = true;
+    if(colorSwitch % 3 === 0){
         memoListItems.forEach((item, index) => {
             if(index % 2 === 0){
                 item.style.backgroundColor = "yellow";
             }else {
                 item.style.backgroundColor = "red";
+            }
+        });
+    }else if(colorSwitch % 3 === 1){
+        memoListItems.forEach((item, index) => {
+            if(index % 2 === 0){
+                item.style.backgroundColor = "red";
+            }else {
+                item.style.backgroundColor = "yellow";
             }
         });
     }else {
-        colorSwitch = false;
         memoListItems.forEach((item, index) => {
             if(index % 2 === 0){
-                item.style.backgroundColor = "red";
+                item.style.backgroundColor = "white";
             }else {
-                item.style.backgroundColor = "yellow";
+                item.style.backgroundColor = "white";
             }
         });
     }
+    colorSwitch ++;
 }
