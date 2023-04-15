63f3df3a11140dbbdec2ecc311e65af8919ead80
koyamal <tanakamaruko1121n@gmail.com>
2023-04-15 17:32:28 +0900

    Add button click function

--- html/src/index2.js
+++ html/src/index2.js
@@ -2,10 +2,26 @@ const memoListItems = document.querySelectorAll('.memo-list-item');
 
 console.log(memoListItems);
 
-memoListItems.forEach((item, index) => {
-    if(index % 2 === 0){
-        item.style.backgroundColor = "yellow";
+let colorSwitch = false;
+
+const onClickColor = () => {
+    if(!colorSwitch){
+        colorSwitch = true;
+        memoListItems.forEach((item, index) => {
+            if(index % 2 === 0){
+                item.style.backgroundColor = "yellow";
+            }else {
+                item.style.backgroundColor = "red";
+            }
+        });
     }else {
-        item.style.backgroundColor = "red";
+        colorSwitch = false;
+        memoListItems.forEach((item, index) => {
+            if(index % 2 === 0){
+                item.style.backgroundColor = "red";
+            }else {
+                item.style.backgroundColor = "yellow";
+            }
+        });
     }
-});
+}
