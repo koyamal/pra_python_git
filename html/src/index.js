1243ff1cf4aba22f1a6ee485b07a991ca6899f2d
koyamal <tanakamaruko1121n@gmail.com>
2023-04-15 16:59:59 +0900

    Add javascript to page

---
+++ html/src/index.js
@@ -0,0 +1,23 @@
+const a = 1;
+
+console.log(a);
+
+let b = 2;
+console.log(b);
+b = 3;
+console.log(b);
+
+const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+}
+
+console.log(obj.a);
+console.log(obj.d);
+
+const array = [1, 2, 3, 4]
+console.log(array);
+console.log(array[1]);
+
